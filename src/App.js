9f1fcb939d023118b108254e43cdfda53417c9bc
Hannah Shin <hs033@bucknell.edu>
2021-08-03 03:21:42 -0400

    small changes to layout and adding an image

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ function App() {
           <div className="favorites">2</div>
           <div className="third-container">
             <div className="customizations">
-              <div className="options"></div>
+              <div className="options">3</div>
               <div classname="add-button">
+              {/* <img src="src\images\add_button.png"></img> */}
               <button type="button" onclick="Testing">Place Holder</button>
               </div>
             </div>
